1229669a27a0eb99a4206e1b1d1f7a404e99acc4
Jxtbrxxd <112715951+Jxtbrxxd@users.noreply.github.com>
2024-12-18 13:54:58 +0000

    Dog_Spa.sql

--- Dog_Spa.sql
+++ Dog_Spa.sql
@@ -3,7 +3,6 @@ CREATE SCHEMA customer1;
 CREATE SCHEMA customer2;
 
 -- Table Creation for customer-specific dog records
-
 CREATE TABLE customer1.dogs(
 dog_id GENERATED ALWAYS AS IDENTITY PRIMARY KEY,-- Auto-incremented ID for dogs
 name GENERATED ALWAYS AS IDENTITY,
@@ -21,11 +20,10 @@ last_grooming DATE
 );
 
 -- Role Creation (Replace placeholders with actual passwords in execution)
-
 CREATE ROLE customer1 WITH LOGIN PASSWORD '<replace_with_secure_password>';
 CREATE ROLE customer2 WITH LOGIN PASSWORD '<replace_with_secure_password>';
---Granting USAGE and read-only(SELECT) permissions to roles
 
+--Granting USAGE and read-only(SELECT) permissions to roles
 GRANT USAGE ON SCHEMA customer1 TO customer1;
 GRANT USAGE ON SCHEMA customer2 TO customer2;
 
@@ -33,12 +31,10 @@ GRANT SELECT ON ALL TABLES IN SCHEMA customer1 TO customer1;
 GRANT SELECT ON ALL TABLES IN SCHEMA customer2 TO customer2;
 
 -- Future-Proof Permissions for new tables
-
 ALTER DEFAULT PRIVILEGES IN SCHEMA customer1 GRANT SELECT ON TABLES TO customer1;
 ALTER DEFAULT PRIVILEGES IN SCHEMA customer2 GRANT SELECT ON TABLES TO customer2;
 
 --Ensuring each ROLE can only interact with their schema by default
-
 ALTER ROLE customer1 SET SEARCH_PATH TO customer1;
 ALTER ROLE customer2 SET SEARCH_PATH TO customer2;
 
